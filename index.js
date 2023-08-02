fc25a6714813e936010bcb44667ac5962d152fb0
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-08-02 19:51:18 +0100

    passing

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+let express = require("express");
+let cors = require("cors");
+require("dotenv").config();
 
-var app = express();
+let app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log("Your app is listening on port " + port);
 });
+
+
+//////////
+const multer = require("multer");
+const upload = multer();
+
+app.post("/api/fileanalyse",
+    upload.single("upfile"), function (req, res) {
+        const { originalname, mimetype, size } = req.file;
+        res.json({ name: originalname, type: mimetype, size: size });
+    });
