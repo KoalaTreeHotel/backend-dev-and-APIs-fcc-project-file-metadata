d3f7a5ced644a168e8aaf4ee536ac315b6e13acc
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-11-30 11:29:14 +0000

    passing all tests (and saving uploaded file to "./public/uploads")

--- index.js
+++ index.js
@@ -23,14 +23,35 @@ app.listen(port, function () {
 // going to be using multer middleware module for file uploading
 // multer usage: https://www.npmjs.com/package/multer
 const myMulter = require("multer");
-const myUpload = myMulter();
+
+let storageInfo = myMulter.diskStorage(
+    {
+        // setting destination for multer uploaded file
+        destination: "./public/uploads/",
+        // prepending unix timestamp to the name of the uploaded file
+        filename: function ( req, file, cb ) {
+            // unix timestamp in seconds (+ unary op convert date to number)
+            const unix = Math.round(+new Date()/1000);
+            cb( null, unix.toString()+"-"+file.originalname);
+        }
+    }
+);
+
+const myUpload = myMulter({"storage": storageInfo});
 
 // multer middleware for uploading a single file goes between
 // the route and the req/res callback. get the name of the file
 // to upload (upfile) from the form in the html form
 app.post("/api/fileanalyse", myUpload.single("upfile"), (req, res) => {
+    // output file info
     console.log("req.file = ");
     console.log(req.file);
     console.log("req.body = ");
     console.log(req.body);
+    // grab fields requested by fcc
+    const theSize = req.file.size;
+    const theName = req.file.originalname;
+    const theType = req.file.mimetype;
+    // respond with json data
+    res.json({ name: theName, type: theType, size: theSize });
 });
