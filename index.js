8a436c886df96d8aa1dac75fe395985c2a0e6d3b
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-11-30 09:59:29 +0000

    multer

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+"use strict";
+
+// SKELETON START
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log("Your app is listening on port " + port);
 });
+// SKELETON END
+
+// going to be using multer middleware module for file uploading
+const myMulter = require("multer");
+const myUpload = myMulter();
